de1927e18bc933bdacbd9aedbdc7484991b70ca9
sahibpreet008 <sahibpreet@eiysys.in>
2021-03-13 15:15:38 +0530

    program done

--- src/screens/countries.tsx
+++ src/screens/countries.tsx
@@ -1,40 +1,82 @@
-import React, { Fragment, useState } from "react";
-
+import React, { Fragment, useState,useEffect } from "react";
+import axios from "axios";
 import { Button, Container, Row, Col, Card, CardBody, CardTitle, Form, FormGroup, Input, ListGroup, ListGroupItem } from "reactstrap";
 
 interface CountryProps {
-  match: any
+  match: any,
+  history:any
 }
+
+
 export default function Countries(props: CountryProps) {
   var country = props.match?.params?.country || "";
-  var [countries, setCountries] = useState<Array<Object>>([]);
+  const [countries, setCountries] = useState<Array<Object>>([]);
+  const [loading,setLoading]=useState(true);
   console.log("Country is ", country);
 
 
-  return (
+  function fetchCountries(country : string){
+    axios.get("https://restcountries.eu/rest/v2/name/"+country)
+    .then((res)=>{
+      console.log("Response received is ",res.data);
+      setCountries(res.data);
+    })
+    .catch((e)=>alert("Error Occured"))
+    .finally(()=>setLoading(false));
+  }
+
+  useEffect(()=>{
+    fetchCountries(country);
+  },[]);
+
+  
+  if(loading){
+    return <div>
+      Loading... Please Wait
+    </div>
+  }
+
+  return (countries.length>0 ? 
+    <Container className="mb-5">
+      {Array.isArray(countries) && countries?.map((item,index)=>
+      <Row className="mt-5">
+        <Col md="6" className={"mx-auto"}>
+          <Card className="w-100 bg-dark">
+            <CardBody>
+              <CardTitle className="text-light">
+                Country : {item.name}
+              </CardTitle>
+              <ListGroup>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">Capital</span>: {item.capital}
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">Population</span>: {item.population}
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">LatLng</span>: {item.latlng[0]} , {item.latlng[1]}
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">Flag</span>: <img className={"w-25"} src={item.flag} />
+                </ListGroupItem>
+              </ListGroup>
+              <Button className="mt-5 float-right" onClick={()=>props.history.push("/weather/"+item.capital)}>Capital Weather</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      )}
+    </Container>
+    :
     <Container>
       <Row className="mt-5">
         <Col md="6" className={"mx-auto"}>
           <Card className="w-100 bg-dark">
             <CardBody>
               <CardTitle className="text-light">
-                Country : {country}
-                        </CardTitle>
-              <ListGroup>
-                <ListGroupItem>
-                  <span className="d-inline-block w-25">Capital</span>:- Delhi
-                </ListGroupItem>
-                <ListGroupItem>
-                <span className="d-inline-block w-25">Population</span>:- Delhi
-                </ListGroupItem>
-                <ListGroupItem>
-                <span className="d-inline-block w-25">LatLng</span>:- Delhi
-                </ListGroupItem>
-                <ListGroupItem>
-                <span className="d-inline-block w-25">Flag</span>:- Delhi
-                </ListGroupItem>
-              </ListGroup>
-              <Button className="mt-5 float-right" onClick={()=>alert("Weather pressed")}>Capital Weather</Button>
+                No Countries Found
+                      </CardTitle>
+              <Button className="mt-5 float-right" onClick={() => props.history.goBack()}>Go Back</Button>
             </CardBody>
           </Card>
         </Col>
--- src/screens/weather.tsx
+++ src/screens/weather.tsx
@@ -1,4 +1,6 @@
-import React,{Fragment} from "react";
+import React, { Fragment, useState,useEffect } from "react";
+import axios from "axios";
+import { Button, Container, Row, Col, Card, CardBody, CardTitle, Form, FormGroup, Input, ListGroup, ListGroupItem } from "reactstrap";
 
 
 interface WeatherProps {
@@ -8,12 +10,62 @@ interface WeatherProps {
 export default function Weather(props:WeatherProps) {
   var capital = props.match?.params?.capital || "";
 
+  var apiKey="82ce4d88c966a0cd9283e61889a06e7f";
+  const [weather,setWeather]=useState(undefined);
+  const [loading,setLoading]=useState(true);
+
+
+  function fetchWeather(capital : string){
+    axios.get(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${capital}`)
+    .then((res)=>{
+      console.log("Response received is ",res.data?.current);
+      setWeather(res.data.current);
+    })
+    .catch((e)=>alert("Error Occured"))
+    .finally(()=>setLoading(false));
+  }
+
+  useEffect(()=>{
+    fetchWeather(capital);
+  },[]);
+
+
   console.log("Capital is  ",capital);
+
+  if(loading){
+    return <div>
+      Loading... Please Wait
+    </div>
+  }
+
     return (
-      <Fragment>
-          <div>
-              Weather
-          </div>
-      </Fragment>
+      <Container className="mb-5">
+      <Row className="mt-5">
+        <Col md="6" className={"mx-auto"}>
+          <Card className="w-100 bg-dark">
+            <CardBody>
+              <CardTitle className="text-light">
+                Weather of Capital : {capital}
+              </CardTitle>
+              <ListGroup>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">temperature</span>: {weather.temperature}
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">weather_icons</span>: <img className={"w-25"} src={weather.weather_icons[0]} />
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">wind_speed</span>: {weather.wind_speed}
+                </ListGroupItem>
+                <ListGroupItem>
+                  <span className="d-inline-block w-25">precip</span>: {Weather.precip || "0"}
+                </ListGroupItem>
+              </ListGroup>
+              <Button className="mt-5 float-right" onClick={()=>props.history.goBack()}>Go Back</Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
     );
   }
